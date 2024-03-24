9277c9e681baa3937a0f575d9c7f2ef3dffdfcb9
WYQ <1150344015@qq.com>
2024-03-24 13:37:38 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,33 @@ int exampleNum = 1;
 vector<unsigned int> seeds;
 vector<string> filenames;
 
-// int main(int argc, char *argv[]) {
-int main() {
+void generateData(int exampleNum) {
     /* --------------------------------- 批量生成数据 --------------------------------- */
     srand((unsigned int) time(NULL));
+    if (exampleNum < 0) {
+        std::cout << "Input the number of test instances: ";
+        std::cin >> exampleNum;
+    }
+    for (int i = 1; i <= exampleNum; i++) {
+        unsigned int seed = rand();
+        seeds.push_back(seed);
+    }
+    for (int i = 1; i <= exampleNum; i++) {
+        DataGenerator dg(dir, 30);
+        dg.generateAndSave(seeds[i - 1], i);
+        std::cout << "instance " << std::to_string(i) << " generated.\n";
+        string filename = dir + "\\" + dg.filenameBase + std::to_string(i) + ".txt";
+        filenames.push_back(filename);
+    }
+}
 
-    // std::cout << "Input the number of test instances: ";
-    // std::cin >> exampleNum;
+void generateData_Solve(int exampleNum) {
+    /* --------------------------------- 批量生成数据 --------------------------------- */
+    srand((unsigned int) time(NULL));
+    if (exampleNum < 0) {
+        std::cout << "Input the number of test instances: ";
+        std::cin >> exampleNum;
+    }
     for (int i = 1; i <= exampleNum; i++) {
         unsigned int seed = rand();
         seeds.push_back(seed);
@@ -47,19 +67,23 @@ int main() {
         ofstream fout;
         fout.open(filename);
 
-        int num = probDisc2D.getLengthDiscNum();
         fout << "distance\tspeed\theight\n";
-
+        int num = probDisc2D.getLengthDiscNum();
         for (int i = 0; i < num; i++) {
             double dis = resource::indexToLength(i, 0, resource::REF_UNIT_LENGTH);
             double hei = resource::indexToHeight(optTraj.getHeightIndex(i), prob2D.getMinHeight(), resource::REF_UNIT_HEIGHT);
             fout << std::to_string(dis) << "\t" << std::to_string(speedSche[i]) << "\t" << std::to_string(hei) << "\n";
         }
         fout.close();
-        // std::cout << "\nTODO: 检查所有变量，尤其是ACOSolver的各个vector，看看size是否正常合理\n";
-        // std::puts("");
-        // cout << "file closed\n";
     }
+}
+
+int main(int argc, char *argv[]) { // int main() {
+    
+    generateData(1);
+    
+    generateData_Solve(1);
+
     // system("pause");
     return 0;
 }

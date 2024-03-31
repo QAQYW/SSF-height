aec09bb7d1fdc6618fe583842e80378d0d5487e8
WYQ <1150344015@qq.com>
2024-03-31 13:52:42 +0800

    new file

---
+++ src/trajectory.cpp
@@ -0,0 +1,59 @@
+#include "trajectory.h"
+
+Trajectory::Trajectory(int size) {
+    heightSche.resize(size, 0);
+}
+
+Trajectory::Trajectory(int size, int heightIndex) {
+    heightSche.resize(size, heightIndex);
+}
+
+void Trajectory::reInit(int size, int heightIndex) {
+    heightSche.resize(size, heightIndex);
+}
+
+void Trajectory::setHeightIndex(int lengthIndex, int heightIndex) {
+    heightSche[lengthIndex] = heightIndex;
+}
+
+void Trajectory::addList(int heightIndex) {
+    heightSche.push_back(heightIndex);
+}
+
+vector<int> Trajectory::getHeightSche() const {
+    return heightSche;
+}
+
+int Trajectory::getHeightIndex(int lengthIndex) const {
+    return heightSche[lengthIndex];
+}
+
+double Trajectory::calHeightCost() const {
+    int size = heightSche.size();
+    double cost = 0;
+    for (int i = 1; i < size; i++) {
+        cost += resource::costByHeight(heightSche[i - 1], heightSche[i]);
+    }
+    return cost;
+}
+
+double Trajectory::calSpeedCost(const ProblemDisc2D &problem2D) const {
+    // double cost = 0;
+    ProblemDisc1D problem1D;
+    problem1D.transformFromProblemDisc2D(problem2D, *this);
+    ssf::SSFSolverDisc ssfSolver(&problem1D);
+    ssfSolver.solve();
+    ssfSolver.calCost();
+    return ssfSolver.getCost();
+}
+
+double Trajectory::calSpeedCost(const ProblemDisc2D &problem2D, vector<double> &speedSche) const {
+    // double cost = 0;
+    ProblemDisc1D problem1D;
+    problem1D.transformFromProblemDisc2D(problem2D, *this);
+    ssf::SSFSolverDisc ssfSolver(&problem1D);
+    ssfSolver.solve();
+    ssfSolver.calCost();
+    speedSche = ssfSolver.getSolution().getSpeedSche();
+    return ssfSolver.getCost();
+}
---
+++ src/trajectory.h
@@ -0,0 +1,38 @@
+#ifndef TRAJECTORY_H
+#define TRAJECTORY_H
+
+#include "resource.h"
+#include "ssf.h"
+// #include "problemDisc2D.h"
+
+// 前置声明
+class ProblemDisc2D;
+
+/**
+ * 只有离散化后的轨迹（高度调度），不涉及速度调度
+*/
+class Trajectory {
+private:
+    // 每个距离对应的高度
+    vector<int> heightSche; // list of height index
+public:
+    Trajectory() {};
+    // 初始化为以固定高度0飞行的轨迹
+    Trajectory(int size);
+    // 初始化为以固定高度heightIndex飞行的轨迹
+    Trajectory(int size, int heightIndex);
+    // 重新初始化
+    void reInit(int size, int heightIndex);
+    void setHeightIndex(int lengthIndex, int heightIndex);
+    void addList(int heightIndex);
+    vector<int> getHeightSche() const;
+    int getHeightIndex(int lengthIdnex) const;
+    // 计算高度变化产生的能耗
+    double calHeightCost() const;
+    // 计算速度调度的能耗（不包含高度变化）
+    double calSpeedCost(const ProblemDisc2D &problem2D) const;
+    // 计算速度调度的能耗（不包含高度变化），并将速度调度由参数speedSche传出
+    double calSpeedCost(const ProblemDisc2D &problem2D, vector<double> &speedSche) const;
+};
+
+#endif

eaab303fb2349a4175ec765f193b46592d56b299
WYQ <1150344015@qq.com>
2024-04-24 18:45:31 +0800

    pso method (offline)

--- src/pso.cpp
+++ src/pso.cpp
@@ -1,1 +1,147 @@
 #include "pso.h"
+
+#include <cmath>
+
+#include "tools.h"
+#include "problemDisc2D.h"
+#include "problemDisc1D.h"
+#include "energy.h"
+
+/* -------------------------------- Partical -------------------------------- */
+
+pso::Partical::Partical(int heightDiscNum, int lengthDiscNum, double gap, const ProblemDisc2D &problem)
+: heightDiscNum(heightDiscNum), lengthDiscNum(lengthDiscNum), gap(gap) {
+    // 随机初始化速度和位置
+    speed.resize(lengthDiscNum);
+    position.resize(lengthDiscNum);
+    bestPosition.resize(lengthDiscNum);
+    for (int i = 0; i < lengthDiscNum; i++) {
+        speed[i] = tools::randDouble(0, 1);
+        bestPosition[i] = position[i] = tools::randDouble(0, 1);
+    }
+    positionToTrajectory();
+    calCost(problem);
+    bestCost = cost;
+
+    trajectory = Trajectory(lengthDiscNum);
+}
+
+void pso::Partical::positionToTrajectory() {
+    int hid;
+    int hMax = heightDiscNum - 1;
+    for (int i = 0; i < lengthDiscNum; i++) {
+        hid = std::min(hMax, (int) (position[i] / gap));
+        trajectory.setHeightIndex(i, hid);
+    }
+}
+
+void pso::Partical::bestPositionToTrajectory() {
+    int hid;
+    int hMax = heightDiscNum - 1;
+    for (int i = 0; i < lengthDiscNum; i++) {
+        hid = std::min(hMax, (int) (bestPosition[i] / gap));
+        trajectory.setHeightIndex(i, hid);
+    }
+}
+
+std::vector<double> pso::Partical::getPosition() const {
+    return position;
+}
+
+std::vector<double> pso::Partical::getBestPosition() const {
+    return bestPosition;
+}
+
+double pso::Partical::getCost() const {
+    return cost;
+}
+
+double pso::Partical::getBestCost() const {
+    return bestCost;
+}
+
+void pso::Partical::updatePosition(double inertia, const Partical &gb) {
+    std::vector<double> gbp = gb.getBestPosition();
+    double rd1 = tools::randDouble(0, 1);
+    double rd2 = tools::randDouble(0, 1);
+    for (int i = 0; i < lengthDiscNum; i++) {
+        speed[i] = inertia * speed[i] + pso::PERSONAL_BEST_COEF * rd1 * (bestPosition[i] - position[i]) + pso::GLOBAL_BEST_COEF * rd2 * (gbp[i] - position[i]);
+        if (speed[i] > pso::MAX_SPEED) speed[i] = pso::MAX_SPEED;
+        if (speed[i] < (-pso::MAX_SPEED)) speed[i] = -pso::MAX_SPEED;
+        position[i] += speed[i];
+        if (position[i] > 1) position[i] = 1;
+        if (position[i] < 0) position[i] = 0;
+    }
+}
+
+void pso::Partical::calCost(const ProblemDisc2D &problem) {
+    hcost = calHeightCost();
+    vcost = calSpeedCost(problem);
+    cost = hcost + vcost;
+    // cost = calHeightCost() + calSpeedCost(problem);
+}
+
+double pso::Partical::calHeightCost() const {
+    return trajectory.calHeightCost();
+}
+
+double pso::Partical::calSpeedCost(const ProblemDisc2D &problem) const {
+    return energy_calculator::calSpeedCost(problem, trajectory);
+}
+
+void pso::Partical::updatePersonalBest() {
+    if (cost < bestCost) {
+        for (int i = 0; i < lengthDiscNum; i++) {
+            bestPosition[i] = position[i];
+        }
+        bestCost = cost;
+    }
+}
+
+/* -------------------------------- PSOSolver ------------------------------- */
+
+pso::PSOSolver::PSOSolver(ProblemDisc2D* prob, int heightDiscNum, int lengthDiscNum)
+: problem(prob), heightDiscNum (heightDiscNum), lengthDiscNum(lengthDiscNum) {
+    
+    swarm.resize(pso::SWARM_SIZE);
+
+    bestPartical = pso::Partical();
+
+    gap = 1.0 / (double) heightDiscNum;
+}
+
+void pso::PSOSolver::solve() {
+    // 初始惯性权重
+    double inertia = pso::INITIAL_INERTIA_VALUE;
+    // 惯性权重每次的减小量（线性）
+    double dInertia = (pso::INITIAL_INERTIA_VALUE - pso::END_INERTIA_VALUE) / pso::MAX_ITERATOR;
+    
+    // 初始化粒子群
+    int bestIndex = 0;
+    for (int i = 0; i < pso::SWARM_SIZE; i++) {
+        swarm[i] = pso::Partical(heightDiscNum, lengthDiscNum, gap, *problem);
+        // swarm[i].calCost(*problem);
+        if (swarm[i].getCost() < swarm[bestIndex].getCost()) bestIndex = i;
+    }
+    bestPartical = pso::Partical(swarm[bestIndex]);
+    
+    int iter = 0;
+    while (iter < pso::MAX_ITERATOR) {
+        bestIndex = 0;
+        for (int i = 0; i < pso::SWARM_SIZE; i++) {
+            swarm[i].updatePosition(inertia, bestPartical); // 包含了速度的更新
+            swarm[i].positionToTrajectory(); // 把连续的position映射为离散的trajectory
+            swarm[i].calCost(*problem);
+            swarm[i].updatePersonalBest();
+            if (swarm[i].getCost() < swarm[bestIndex].getCost()) bestIndex = i;
+        }
+
+        // 更新全局最优粒子（主要是位置）
+        if (swarm[bestIndex].getCost() < bestPartical.getCost()) {
+            bestPartical = pso::Partical(swarm[bestIndex]);
+        }
+
+        iter++;
+        inertia -= dInertia;
+    }
+}
--- src/pso.h
+++ src/pso.h
@@ -1,7 +1,86 @@
 #ifndef PSO_H
 #define PSO_H
 
+#include <vector>
 
+#include "trajectory.h"
+
+// 前置声明
+class ProblemDisc2D;
+
+// 离散粒子群优化算法，BPSO
+namespace pso {
+
+// some parameters
+extern const double INITIAL_INERTIA_VALUE;
+extern const double END_INERTIA_VALUE;
+extern const double PERSONAL_BEST_COEF;
+extern const double GLOBAL_BEST_COEF;
+extern const int SWARM_SIZE;
+extern const double MAX_SPEED;
+extern const int MAX_ITERATOR;
+
+// extern std::vector<double> pso_MapTable;
+
+/// @brief 粒子
+class Partical {
+private:
+    int heightDiscNum;
+    int lengthDiscNum;
+    /// @brief 无人机能耗作为粒子适应度值, cost = hcost + vcost
+    double cost;
+    double hcost;
+    double vcost;
+    /// @brief 粒子速度
+    std::vector<double> speed;
+    /// @brief 粒子位置
+    std::vector<double> position;
+    /// @brief 粒子到过的最优位置
+    std::vector<double> bestPosition;
+    /// @brief 粒子到过的最优位置（bestPosition）的适应度值
+    double bestCost;
+    Trajectory trajectory;
+    double gap;
+
+public:
+    Partical() {};
+    Partical(int heightDiscNum, int lengthDiscNum, double gap, const ProblemDisc2D &problem);
+    /// @brief 把位置position映射为离散的路径解trajectory
+    void positionToTrajectory();
+    /// @brief 把bestPosition映射为离散的路径解trajectory
+    void bestPositionToTrajectory();
+    std::vector<double> getPosition() const;
+    std::vector<double> getBestPosition() const;
+    double getCost() const;
+    double getBestCost() const;
+    /// @brief 更新speed和position
+    /// @param gb 全局最优解
+    void updatePosition(double inertia, const Partical &gb);
+    void calCost(const ProblemDisc2D &problem);
+    double calHeightCost() const;
+    double calSpeedCost(const ProblemDisc2D &problem) const;
+    /// @brief 更新个体最优位置
+    void updatePersonalBest();
+};
+
+class PSOSolver {
+private:
+    ProblemDisc2D* problem;
+    int heightDiscNum;
+    int lengthDiscNum;
+    std::vector<Partical> swarm;
+    Partical bestPartical;
+    /// @brief 用于把位置映射成trajectory的
+    double gap;
+
+public:
+    PSOSolver(ProblemDisc2D* prob, int heightDiscNum, int lengthDiscNum);
+    void solve();
+
+
+};
+
+} // namespace pso
 
 
 #endif

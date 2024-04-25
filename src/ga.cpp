662e37295946d17a12babfacb6832c6b6de9b515
WYQ <1150344015@qq.com>
2024-04-25 20:51:15 +0800

    update GA method

--- src/ga.cpp
+++ src/ga.cpp
@@ -1,5 +1,9 @@
 #include "ga.h"
 
+#include <algorithm>
+#include <random>
+#include <ctime>
+
 #include "trajectory.h"
 #include "problemDisc2D.h"
 #include "problemDisc1D.h"
@@ -9,8 +13,19 @@
 
 /* ------------------------------- Individual ------------------------------- */
 
-ga::Individual::Individual() {
-    // TODO: complete
+ga::Individual::Individual(int heightDiscNum, int lengthDiscNum) : heightDiscNum(heightDiscNum), lengthDiscNum(lengthDiscNum) {
+    // hmin = 0
+    // hmax = heightDiscNum - 1
+    trajectory = Trajectory(lengthDiscNum);
+    for (int i = 0, h; i < lengthDiscNum; i++) {
+        h = tools::randInt(0, heightDiscNum - 1);
+        trajectory.setHeightIndex(i, h);
+    }
+}
+
+ga::Individual::Individual(int heightDiscNum, int lengthDiscNum, Trajectory traj)
+: heightDiscNum(heightDiscNum), lengthDiscNum(lengthDiscNum), trajectory(traj) {
+    // empty
 }
 
 Trajectory ga::Individual::getTrajectory() const {
@@ -47,8 +62,94 @@ void ga::Individual::mutation() {
     }
 }
 
+bool ga::Individual::operator< (const ga::Individual& _individual) const {
+    return cost < _individual.cost;
+    // return cost < _individual.getCost();
+}
+
 /* -------------------------------- GASovler -------------------------------- */
 
 ga::GASolver::GASolver(ProblemDisc2D *prob) : problem(prob) {
     // TODO: complete
+    heightDiscNum = problem->getHeightDiscNum();
+    lengthDiscNum = problem->getLengthDiscNum();
+
+    trajectory = Trajectory(lengthDiscNum);
+}
+
+double ga::GASolver::getCost() const {
+    return cost;
+}
+
+Trajectory ga::GASolver::getTrajectory() const {
+    return trajectory;
+}
+
+void ga::GASolver::solve() {
+    Population parents;
+    int bestIndex = 0;
+    for (int i = 0; i < ga::POPULATION_SIZE; i++) {
+        parents.push_back(ga::Individual(heightDiscNum, lengthDiscNum));
+        parents[i].calCost(*problem);
+        if (parents[i].getCost() < parents[bestIndex].getCost()) {
+            bestIndex = i;
+        }
+    }
+
+    trajectory = Trajectory(parents[bestIndex].getTrajectory());
+    cost = parents[bestIndex].getCost();
+
+    // 用于给parents配对
+    int indices[ga::POPULATION_SIZE];
+    for (int i = 0; i < ga::POPULATION_SIZE; i++) indices[i] = i;
+
+    int iter = 0;
+    while (iter < ga::MAX_ITERATOR) {
+        Population children;
+        // todo 交叉
+        std::shuffle(indices, indices + ga::POPULATION_SIZE, std::default_random_engine(std::time(NULL)));
+        for (int i = 0; i < ga::POPULATION_SIZE; i += 2) {
+            // parents[indices[i]] 和 parents[indices[i + 1]] 进行交叉
+            crossover(children, parents[indices[i]], parents[indices[i + 1]]);
+        }
+        // todo 变异
+        for (int i = 0; i < ga::POPULATION_SIZE; i++) {
+            parents[i].mutation();
+            children[i].mutation();
+            parents[i].calCost(*problem);
+            children[i].calCost(*problem);
+        }
+        // todo 选择
+        selection(parents, children);
+        // todo 更新最优trajectory
+        if (parents[0].getCost() < cost) {
+            trajectory = Trajectory(parents[0].getTrajectory());
+            cost = parents[0].getCost();
+        }
+        ++iter;
+    }
+}
+
+void ga::GASolver::crossover(ga::Population &children, ga::Individual p1, ga::Individual p2) const {
+    Trajectory traj1 = p1.getTrajectory();
+    Trajectory traj2 = p2.getTrajectory();
+    // 交叉的区间
+    int lpos = tools::randInt(0, lengthDiscNum - 1);
+    int rpos = tools::randInt(0, lengthDiscNum - 1);
+    if (lpos < rpos) std::swap(lpos, rpos);
+    // 交换高度
+    for (int i = lpos, temp; i <= rpos; i++) {
+        temp = traj1.getHeightIndex(i);
+        traj1.setHeightIndex(i, traj2.getHeightIndex(i));
+        traj2.setHeightIndex(i, temp);
+    }
+    // 加入children中
+    children.push_back(ga::Individual(heightDiscNum, lengthDiscNum, traj1));
+    children.push_back(ga::Individual(heightDiscNum, lengthDiscNum, traj2));
+}
+
+void ga::GASolver::selection(ga::Population &parents, const ga::Population &children) const {
+    parents.insert(parents.end(), children.begin(), children.end());
+    sort(parents.begin(), parents.end());
+    parents.erase(parents.begin() + ga::POPULATION_SIZE, parents.end());
 }
--- src/ga.h
+++ src/ga.h
@@ -11,6 +11,8 @@ class ProblemDisc2D;
 // 遗传算法
 namespace ga {
 
+extern int const MAX_ITERATOR; // 最大迭代次数
+extern int const POPULATION_SIZE; // 种群规模，必须偶数
 extern double const MUTATION_PROBABILITY; // 变异概率 0.005
 
 // 个体
@@ -22,7 +24,15 @@ private:
     Trajectory trajectory;
 
 public:
-    Individual();
+    /// @brief 构造函数。随机生成一个trajectory
+    /// @param heightDiscNum 离散高度值的数量
+    /// @param lengthDiscNum 离散距离值的数量（=路径长度）
+    Individual(int heightDiscNum, int lengthDiscNum);
+    /// @brief 构造函数
+    /// @param heightDiscNum 离散高度值的数量
+    /// @param lengthDiscNum 离散距离值的数量（=路径长度）
+    /// @param traj 无人机飞行路径
+    Individual(int heightDiscNum, int lengthDiscNum, Trajectory traj);
     Trajectory getTrajectory() const;
     double getCost() const;
     void calCost(const ProblemDisc2D &problem);
@@ -30,6 +40,8 @@ public:
     double calSpeedCost(const ProblemDisc2D &problem) const;
     /// @brief 变异操作。每个位置都有固定的概率变异为其他的飞行高度。
     void mutation();
+
+    bool operator< (const Individual& _individual) const;
 };
 
 // 种群-个体Individual的集合
@@ -39,9 +51,18 @@ typedef std::vector<Individual> Population;
 class GASolver {
 private:
     ProblemDisc2D *problem;
+    int heightDiscNum;
+    int lengthDiscNum;
+    Trajectory trajectory;
+    double cost;
 
 public:
     GASolver(ProblemDisc2D *prob);
+    double getCost() const;
+    Trajectory getTrajectory() const;
+    void solve();
+    void crossover(Population &children, Individual p1, Individual p2) const;
+    void selection(Population &parents, const Population &children) const;
 };
 
 

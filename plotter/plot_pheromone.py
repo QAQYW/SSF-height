411e583dad7fa463ef2bfb88e7d6b950d3bcc3cc
WYQ <1150344015@qq.com>
2024-12-02 22:04:37 +0800

    read from file

--- plotter/plot_pheromone.py
+++ plotter/plot_pheromone.py
@@ -1,5 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 if __name__ == '__main__':
-    print("visualization of pheromone matrix\n")
+    # 蚁群算法的信息素可视化
+    path = "D:\\VSCodeWorkspace\\SSF-height\\newnewexp\\exp000\\pheromone_copy.txt"
+    pheromone = []
+    try:
+        with open(file=path, mode="r") as file:
+            buff = file.readline().strip()
+            dims = [int(_) for _ in buff.split('\t')]
+            # 输入的信息素维度
+            print("dims =", dims)
+            for i in range(dims[0]):
+                tmp = []
+                for j in range(dims[1]):
+                    buff = file.readline().strip()
+                    tmp.append([float(_) for _ in buff.split('\t')])
+                pheromone.append(tmp)
+    except FileNotFoundError:
+        print("File not found: " + path)
+    # 检查pheromone是否与输入大小一致
+    print("shape =", np.array(pheromone).shape)
+    

e0ca78d2364518c0862b0a2804d7e3d52a88d174
WYQ <1150344015@qq.com>
2024-12-05 17:47:16 +0800

    update

--- plotter/plot_pheromone.py
+++ plotter/plot_pheromone.py
@@ -15,6 +15,23 @@ def pheromone_to_alpha(pheromone, dims):
     # print(alphas)
     return alphas
 
+def pheromone_to_color_depth(pheromone, dims):
+    depth_min = 30
+    depth_set = np.zeros(dims)
+    ph_max = np.max(pheromone)
+    ph_min = np.min(pheromone)
+    ph_span = ph_max - ph_min
+    print(ph_max, ph_min, ph_span)
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            for k in range(dims[2]):
+                depth_set[i][j][k] = (pheromone[i][j][k] - ph_min) / ph_span
+    return depth_set
+
+def cal_rgb(color, base, rate):
+    new_color = color * base + (color - color * base) * rate
+    return new_color
+
 if __name__ == '__main__':
     # 蚁群算法的信息素可视化
     path = "D:\\VSCodeWorkspace\\SSF-height\\newnewexp\\exp000\\pheromone_copy.txt"
@@ -37,16 +54,31 @@ if __name__ == '__main__':
     print("dims =", dims)
     # 检查pheromone是否与输入大小一致
     print("shape =", pheromone.shape)
-    color = 'orange'
-    alphas = pheromone_to_alpha(pheromone, dims)
+
+    depth_min = 0.1
+    depth_set = pheromone_to_color_depth(pheromone, dims)
     for k in range(dims[2]):
-        plt.plot([-1,0], [0,k], color=color, alpha=alphas[0][0][k])
+        r = cal_rgb(255, depth_min, depth_set[0][0][k]) / 255.0
+        g = cal_rgb(165, depth_min, depth_set[0][0][k]) / 255.0
+        b = 0
+        plt.plot([-1,0], [0,k], color=(r,g,b))
     for i in range(1,dims[0]):
         for j in range(dims[1]):
             for k in range(dims[2]):
-                plt.plot([i-1, i], [j, k], color=color, alpha=alphas[i][j][k])
-    print('prepare to save figure')
-    save_path = "D:\\Desktop\\ssf-journal\\figure\\pheromone visualization\\orange_3-2.pdf"
-    plt.savefig(save_path)
-    print('have saved figure: ' + save_path)
+                r = cal_rgb(255, depth_min, depth_set[i][j][k]) / 255.0
+                g = cal_rgb(165, depth_min, depth_set[i][j][k]) / 255.0
+                b = 0
+                plt.plot([i-1,i], [j,k], color=(r,g,b))
+    # color = 'orange'
+    # alphas = pheromone_to_alpha(pheromone, dims)
+    # for k in range(dims[2]):
+    #     plt.plot([-1,0], [0,k], color=color, alpha=alphas[0][0][k])
+    # for i in range(1,dims[0]):
+    #     for j in range(dims[1]):
+    #         for k in range(dims[2]):
+    #             plt.plot([i-1, i], [j, k], color=color, alpha=alphas[i][j][k])
+    # print('prepare to save figure')
+    # save_path = "D:\\Desktop\\ssf-journal\\figure\\pheromone visualization\\orange_3-2.pdf"
+    # plt.savefig(save_path)
+    # print('have saved figure: ' + save_path)
     plt.show()
